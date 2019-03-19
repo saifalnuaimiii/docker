3e297b7c379478353798ff3fa150ab16abc1e06a
saifalnuaimiii <saif.al-nuaimi@progressoft.com>
2019-03-19 08:42:51 +0200

    Frontend Frist App

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-         Hello Yazan and Sami 
+         Hello abdalah
 	    <a
             className="App-link"
             href="https://reactjs.org"
